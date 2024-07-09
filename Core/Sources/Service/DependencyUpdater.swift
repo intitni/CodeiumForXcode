578b7eb5ae3861193aec8f9a13ba25fcf48dcb96
Shx Guo <int123c@gmail.com>
2024-07-09 15:58:12 +0800

    Update DependencyUpdater to always install language server

--- Core/Sources/Service/DependencyUpdater.swift
+++ Core/Sources/Service/DependencyUpdater.swift
@@ -9,45 +9,9 @@ struct DependencyUpdater {
     func update() {
         Task {
             await withTaskGroup(of: Void.self) { taskGroup in
-                let gitHubCopilot = GitHubCopilotInstallationManager()
-                switch gitHubCopilot.checkInstallation() {
-                case .notInstalled: break
-                case .installed: break
-                case .unsupported: break
-                case .outdated:
-                    taskGroup.addTask {
-                        do {
-                            for try await step in gitHubCopilot.installLatestVersion() {
-                                let state = {
-                                    switch step {
-                                    case .downloading:
-                                        return "Downloading"
-                                    case .uninstalling:
-                                        return "Uninstalling old version"
-                                    case .decompressing:
-                                        return "Decompressing"
-                                    case .done:
-                                        return "Done"
-                                    }
-                                }()
-                                Logger.service
-                                    .error("Update GitHub Copilot language server: \(state)")
-                            }
-                        } catch {
-                            Logger.service.error(
-                                "Update GitHub Copilot language server: \(error.localizedDescription)"
-                            )
-                        }
-                    }
-                }
-
                 let codeium = CodeiumInstallationManager()
 
-                switch await codeium.checkInstallation() {
-                case .notInstalled: break
-                case .installed: break
-                case .unsupported: break
-                case .outdated:
+                func install() {
                     taskGroup.addTask {
                         do {
                             for try await step in codeium.installLatestVersion() {
@@ -71,6 +35,13 @@ struct DependencyUpdater {
                             )
                         }
                     }
+                }
+
+                switch await codeium.checkInstallation() {
+                case .notInstalled: install()
+                case .installed: break
+                case .unsupported: break
+                case .outdated: install()
                 }
             }
         }
